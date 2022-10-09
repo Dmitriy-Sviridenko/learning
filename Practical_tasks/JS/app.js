97c71ddb25104c15cd37680c25603387ac203cd3
Dmitriy Sviridenko <svi.dim305@gmail.com>
2022-10-09 21:47:41 +0300

    09.10.2022

--- Practical_tasks/JS/app.js
+++ Practical_tasks/JS/app.js
@@ -1,28 +1,21 @@
-function greet(language) {
-	switch (language){
-	  case "IP_ADDRESS_INVALID" : return 'Welcome'
-      case "english" : return 'Welcome'
-	  case "czech" :  return 'Vitejte'
-	  case "danish" : return 'Velkomst'
-	  case "dutch" : return 'Welkom'
-	  case "estonian" : return 'Tere tulemast'
-	  case "finnish" : return 'Tervetuloa'
-	  case "flemish" : return 'Welgekomen'
-	  case "french" : return 'Bienvenue'
-	  case "german" : return 'Willkommen'
-	  case "irish" : return 'Failte'
-	  case "italian" : return 'Benvenuto'
-	  case "latvian" : return 'Gaidits'
-	  case "lithuanian" : return 'Laukiamas'
-	  case "polish" : return 'Witamy'
-	  case "spanish" : return 'Bienvenido'
-	  case "swedish" : return 'Valkommen'
-	  case "welsh" : return 'Croeso'
-  }
+function descendingOrder(n){
+	return Number(sort(String(n).split("")).join(""));
+}
+
+function sort(n){
+	for (j = n.length - 1; j > 0; j--){
+		for ( i = 0; i < j; i++){
+			if (n[i] < n[i+1]){
+				let temp = n[i];
+				n[i] = n[i+1];
+				n[i+1] = temp;
+			}
+		}
+	}
+	return 	n
 }
 
 
-
-console.log(greet('czech'))
+console.log(descendingOrder(111))
 
 
