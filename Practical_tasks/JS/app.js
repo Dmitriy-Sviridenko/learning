f837bcafc59541e4d0d7b0588c42169a6a22bc85
Dmitriy Sviridenko <svi.dim305@gmail.com>
2022-12-02 20:45:37 +0300

    02/12/2022

--- Practical_tasks/JS/app.js
+++ Practical_tasks/JS/app.js
@@ -1,21 +1,7 @@
-function descendingOrder(n){
-	return Number(sort(String(n).split("")).join(""));
-}
-
-function sort(n){
-	for (j = n.length - 1; j > 0; j--){
-		for ( i = 0; i < j; i++){
-			if (n[i] < n[i+1]){
-				let temp = n[i];
-				n[i] = n[i+1];
-				n[i+1] = temp;
-			}
-		}
-	}
-	return 	n
+function arrayDiff(a, b) {
+  
 }
 
 
-console.log(descendingOrder(111))
-
+console.log(sumStr("2", "3"))
 
