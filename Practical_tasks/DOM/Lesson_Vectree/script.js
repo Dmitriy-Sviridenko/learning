97a46891106fd40f27e0bcc5637d43a4c45e2c13
Dmitriy Sviridenko <svi.dim305@gmail.com>
2022-12-30 20:32:55 +0300

    30/12/2022

---
+++ Practical_tasks/DOM/Lesson_Vectree/script.js
@@ -0,0 +1,15 @@
+const container = document.querySelector("#container")
+
+const controls = document.querySelector(".controls")
+
+const divs = document.querySelectorAll("div")
+
+const div = document.createElement("div")
+
+container.append(div)
+
+div.classList.add('fisrt')
+
+container.removeChild(div)
+
+console.log(divs)
