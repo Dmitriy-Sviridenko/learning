ae6d8ec1420887e694ef698da857c12fec0989f3
Dmitriy Sviridenko <svi.dim305@gmail.com>
2023-02-04 16:09:24 +0300

    04.02.2023 pop-up correct

--- Layout_13.09.2022/js/scipt.js
+++ Layout_13.09.2022/js/scipt.js
@@ -41,16 +41,32 @@ new Swiper(".testimonials__reviews", {
     
   });
 
+
+
+const body = document.querySelector("body")
 const openPopUp = document.querySelectorAll(".pop-up__button").forEach(
     item => item.addEventListener("click", function(e){
     e.preventDefault(); //отключение переходов по ссылке
     popUp.classList.add("active");
+    body.classList.add("swith_off_body");
   })
 )
 
 const closePopUp = document.querySelector(".pop_up_close");
 const popUp = document.querySelector(".pop_up");
+const popUpBackground = document.querySelector(".pop_up_container");
+
+
+//закрытие поп-ап по крестику
 closePopUp.addEventListener("click", () =>{
   popUp.classList.remove("active");
+  body.classList.remove("swith_off_body");
 })
 
+//закрытие поп-ап по оверлею
+document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+  if(e.target === popUpBackground) {
+    popUp.classList.remove("active");
+    body.classList.remove("swith_off_body");
+  }
+});
