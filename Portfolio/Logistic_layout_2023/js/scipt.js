05839a5932b37035d80af227fba854c8f871dd4d
Dmitriy Sviridenko <svi.dim305@gmail.com>
2023-03-12 20:25:59 +0300

    12.03.2023

--- Portfolio/Logistic_layout_2023/js/scipt.js
+++ Portfolio/Logistic_layout_2023/js/scipt.js
@@ -1,3 +1,5 @@
+// services start
+
 const tabsBtn = document.querySelectorAll(".services__item")
 const tabsItems = document.querySelectorAll(".services__info")
 
@@ -26,3 +28,37 @@ tabsBtn.forEach(function(item){
 
 //вирутальный клик по первому элементу (чтобы не прописывать в html класс вручную)
 document.querySelector(".services__item").click(); 
+
+// services end
+
+// pop-up start
+
+const body = document.querySelector("body")
+const openPopUp = document.querySelectorAll(".pop-up__button").forEach(
+    item => item.addEventListener("click", function(e){
+    e.preventDefault(); //отключение переходов по ссылке
+    popUp.classList.add("active");
+    body.classList.add("swith_off_body");
+  })
+)
+
+const closePopUp = document.querySelector(".pop_up_close");
+const popUp = document.querySelector(".pop_up");
+const popUpBackground = document.querySelector(".pop_up_container");
+
+
+//закрытие поп-ап по крестику
+closePopUp.addEventListener("click", () =>{
+  popUp.classList.remove("active");
+  body.classList.remove("swith_off_body");
+})
+
+//закрытие поп-ап по оверлею
+document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+  if(e.target === popUpBackground) {
+    popUp.classList.remove("active");
+    body.classList.remove("swith_off_body");
+  }
+});
+
+// pop-up end
