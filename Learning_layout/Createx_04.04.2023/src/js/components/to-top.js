7389b9a98781ced44a3efe4b3691d8749c11113a
Dmitriy Sviridenko <svi.dim305@gmail.com>
2023-07-05 23:57:46 +0300

    Createx additional new modals

--- Learning_layout/Createx_04.04.2023/src/js/components/to-top.js
+++ Learning_layout/Createx_04.04.2023/src/js/components/to-top.js
@@ -1,25 +1,27 @@
 const toTop = document.querySelector(".to-top");
 let heroHeight;
 
-if (document.querySelector(".hero")) {
-  heroHeight = document.querySelector(".hero").offsetHeight;
-};
-
-if (document.querySelector(".page-hero")) {
-  heroHeight = document.querySelector(".page-hero").offsetHeight;
-}
-
-const isVisivleToTop = (y = 0) => {
-  if (y >= heroHeight) {
-    toTop.classList.add("to-top--active");
-  } else {
-    toTop.classList.remove("to-top--active");
+if (toTop) {
+  if (document.querySelector(".hero")) {
+    heroHeight = document.querySelector(".hero").offsetHeight;
+  };
+  
+  if (document.querySelector(".page-hero")) {
+    heroHeight = document.querySelector(".page-hero").offsetHeight;
   }
+  
+  const isVisivleToTop = (y = 0) => {
+    if (y >= heroHeight) {
+      toTop.classList.add("to-top--active");
+    } else {
+      toTop.classList.remove("to-top--active");
+    }
+  }
+  
+  isVisivleToTop(window.scrollY);
+  
+  window.addEventListener("scroll", () => {
+    let y = window.scrollY;
+    isVisivleToTop(y);
+  });
 }
-
-isVisivleToTop(window.scrollY);
-
-window.addEventListener("scroll", () => {
-  let y = window.scrollY;
-  isVisivleToTop(y);
-});
