4655b3ea677dc0d3ee3553d3d07693e637e1d3b6
Dmitriy Sviridenko <svi.dim305@gmail.com>
2024-01-25 22:02:05 +0300

    Panto final: blocks + footer

--- Learning_layout/Panto_07.01.2024/js/main.js
+++ Learning_layout/Panto_07.01.2024/js/main.js
@@ -19,4 +19,74 @@ document,addEventListener("click", function () {
 //Запрещаем всплытие события клика при клике на подсказку
 for (let hint of infoHints) {
   hint.addEventListener("click", (e) => e.stopPropagation());
+};
+
+// Siper slider
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  freeMode: true,
+
+  slidesPerView: 4,
+  spaceBetween: 42,
+
+  // breakpoints: {
+  //   640: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20,
+  //   },
+  //   768: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 40,
+  //   },
+  //   1024: {
+  //     slidesPerView: 5,
+  //     spaceBetween: 50,
+  //   },
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '#sliderNext',
+    prevEl: '#sliderPrev',
+  },
+});
+
+
+// Tabs
+
+const tabsBtns = document.querySelectorAll(`[data-tab]`);
+const tabsProducts = document.querySelectorAll(`[data-tab-value]`);
+
+for (let btn of tabsBtns) {
+  btn.addEventListener("click", function () {
+    
+    for (let btn of tabsBtns) {
+      btn.classList.remove("tab-contols__btn--active");
+    };
+
+    this.classList.add("tab-contols__btn--active");
+
+    //Получаем значение категории товаров, которые нужно включить
+
+    // Отоброжение и скрытие товаров
+    for (let product of tabsProducts) {
+
+      if (this.dataset.tab === "all") {
+        product.classList.remove("none");
+      } else {
+        if (product.dataset.tabValue === this.dataset.tab) {
+          product.classList.remove("none");
+        } else {
+          product.classList.add("none");
+        };
+      }
+    };
+
+    //Update Swiper
+    swiper.update()
+  });
 }
+
+//tab-contols__btn--active
+
